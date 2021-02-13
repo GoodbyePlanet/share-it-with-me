5223efc316bb77429f2fb7678e9df93c3d359e9e
Nemanja <nemanja.vasic@codecentric.de>
2021-02-13 22:12:38 +0100

    WIP use specific test data for every test file

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,133 +1,137 @@
 import {App} from "../src/startServer";
-import request from "graphql-request";
+import {GraphQLClient} from "graphql-request";
 import {AddressInfo} from "net";
 import {login, signUp} from "./graphql";
-import {CreateUserInput, LoginUserInput} from "../resolvers-example";
-import {cleanTestData} from "./testData";
+import {cleanTestData, createTestData} from "./testData";
 
+let graphQLClient: GraphQLClient;
 let getHost = (): string => "";
 
 beforeAll(async () => {
   const app = await App();
   const {port} = app.address() as AddressInfo;
   getHost = () => `http://localhost:${port}/graphql`;
+  await createTestData();
 });
 
 afterAll(async () => {
   await cleanTestData();
 });
 
+beforeEach((): void => {
+  graphQLClient = new GraphQLClient(getHost());
+});
+
 describe("Auth", (): void => {
+  // describe("SignUp", (): void => {
+  const testUser = "TestUser";
+  const testUserEmail = "testuser@gmail.com";
 
-  describe("SignUp", (): void => {
-    const testUser = "TestUser";
-    const testUserEmail = "testuser@gmail.com";
+  const userSignUpMutationVariables = {
+    user: {
+      email: testUserEmail,
+      username: testUser,
+      password: "SuperCoolPassword"
+    }
+  };
 
-    const userSignUpMutationVariables = {
-      user: {
-        email: testUserEmail,
-        username: testUser,
-        password: "SuperCoolPassword"
-      } as CreateUserInput
-    };
+  it("should create user", async (): Promise<void> => {
+    const signUpResponse = await graphQLClient.request(signUp, userSignUpMutationVariables);
 
-    it("should create user", async (): Promise<void> => {
-      const signUpResponse = await request(getHost(), signUp, userSignUpMutationVariables);
+    expect(signUpResponse).toHaveProperty("signup");
+    expect(signUpResponse.signup.user.username).toBe(testUser);
+    expect(signUpResponse.signup.user.email).toBe(testUserEmail);
+  });
 
-      expect(signUpResponse).toHaveProperty("signup");
-      expect(signUpResponse.signup.user.username).toBe(testUser);
-      expect(signUpResponse.signup.user.email).toBe(testUserEmail);
+  it("should return `Email already taken!` error when attempting to create user with email that is already taken",
+    async (): Promise<void> => {
+      const signUpResponse = async () => {
+        await graphQLClient.request(signUp, userSignUpMutationVariables)
+      };
+
+      await expect(signUpResponse).rejects.toThrow("Email already taken!");
     });
 
-    it("should return `Email already taken!` error when attempting to create user with email that is already taken",
-      async (): Promise<void> => {
-        const signUpResponse = async () => {
-          await request(getHost(), signUp, userSignUpMutationVariables)
-        };
+  it("should return `Please use valid email` error if email is not valid", async (): Promise<void> => {
+    const invalidMutationInput = {
+      ...userSignUpMutationVariables,
+      user: {
+        ...userSignUpMutationVariables.user,
+        email: "test@test"
+      }
+    }
+    const signUpResponse = async () => {
+      await graphQLClient.request(signUp, invalidMutationInput)
+    };
 
-        await expect(signUpResponse).rejects.toThrow("Email already taken!");
-      });
+    await expect(signUpResponse).rejects.toThrow("Please use valid email");
+  });
 
-    it("should return `Please use valid email` error if email is not valid", async (): Promise<void> => {
+  it("should return `Password has to be at least 6 characters long` error if password does not have valid length",
+    async (): Promise<void> => {
       const invalidMutationInput = {
         ...userSignUpMutationVariables,
         user: {
           ...userSignUpMutationVariables.user,
-          email: "test@test"
+          password: "Pass"
         }
       }
       const signUpResponse = async () => {
-        await request(getHost(), signUp, invalidMutationInput)
+        await graphQLClient.request(signUp, invalidMutationInput)
       };
 
-      await expect(signUpResponse).rejects.toThrow("Please use valid email");
+      await expect(signUpResponse).rejects.toThrow("Password has to be at least 6 characters long");
     });
+  // });
 
-    it("should return `Password has to be at least 6 characters long` error if password does not have valid length",
-      async (): Promise<void> => {
-        const invalidMutationInput = {
-          ...userSignUpMutationVariables,
-          user: {
-            ...userSignUpMutationVariables.user,
-            password: "Pass"
-          }
-        }
-        const signUpResponse = async () => {
-          await request(getHost(), signUp, invalidMutationInput)
-        };
+  // describe("Login", (): void => {
+  //   const testUserEmail = "testuser@gmail.com";
+  const userLoginMutationVariables = {
+    user: {
+      email: testUserEmail,
+      password: "SuperCoolPassword"
+    }
+  };
 
-        await expect(signUpResponse).rejects.toThrow("Password has to be at least 6 characters long");
-      });
+  it("should successfully login the user", async (): Promise<void> => {
+    const loginResponse = await graphQLClient.request(login, userLoginMutationVariables);
+
+    expect(loginResponse).toHaveProperty("login");
+    expect(loginResponse.login.token).not.toBeNull();
+    expect(loginResponse.login.user.email).toBe(testUserEmail);
   });
 
-  describe("Login", (): void => {
-    const testUserEmail = "testuser@gmail.com";
-    const userLoginMutationVariables = {
+  it("should return `User not found!` error if user does not exists", async (): Promise<void> => {
+    const wrongEmailUserLogin = {
+      ...userLoginMutationVariables,
       user: {
-        email: testUserEmail,
-        password: "SuperCoolPassword"
-      } as LoginUserInput
+        ...userLoginMutationVariables.user,
+        email: "wrongemail@gmail.com"
+      }
     };
 
-    it("should successfully login the user", async (): Promise<void> => {
-      const loginResponse = await request(getHost(), login, userLoginMutationVariables);
+    const loginResponse = async () => {
+      await graphQLClient.request(login, wrongEmailUserLogin)
+    };
 
-      expect(loginResponse).toHaveProperty("login");
-      expect(loginResponse.login.token).not.toBeNull();
-      expect(loginResponse.login.user.email).toBe(testUserEmail);
-    });
+    await expect(loginResponse).rejects.toThrow("User not found!");
+  });
 
-    it("should return `User not found!` error if user does not exists", async (): Promise<void> => {
-      const wrongEmailUserLogin = {
+  it("should return `Provided credentials are invalid!` error if password is incorrect",
+    async (): Promise<void> => {
+      const wrongCredentialsUserLogin = {
         ...userLoginMutationVariables,
         user: {
           ...userLoginMutationVariables.user,
-          email: "wrongemail@gmail.com"
-        }
+          password: "super cool password"
+        },
       };
 
       const loginResponse = async () => {
-        await request(getHost(), login, wrongEmailUserLogin)
+        await graphQLClient.request(login, wrongCredentialsUserLogin)
       };
 
-      await expect(loginResponse).rejects.toThrow("User not found!");
+      await expect(loginResponse).rejects.toThrow("Provided credentials are invalid!");
     });
-
-    it("should return `Provided credentials are invalid!` error if password is incorrect",
-      async (): Promise<void> => {
-        const wrongCredentialsUserLogin = {
-          ...userLoginMutationVariables,
-          user: {
-            ...userLoginMutationVariables.user,
-            password: "super cool password"
-          },
-        };
-
-        const loginResponse = async () => {
-          await request(getHost(), login, wrongCredentialsUserLogin)
-        };
-
-        await expect(loginResponse).rejects.toThrow("Provided credentials are invalid!");
-      });
-  });
+  // });
 });
--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,72 +1,71 @@
-import {GraphQLClient} from "graphql-request";
-import {AddressInfo} from "net";
-import {cleanTestData, createTestData} from "./testData";
-import {App} from "../src/startServer";
-import {login, users} from "./graphql";
-
-let getHost = (): string => "";
-let graphQLClient: GraphQLClient;
-
-beforeAll(async (): Promise<void> => {
-  const app = await App();
-  const {port} = app.address() as AddressInfo;
-  getHost = () => `http://localhost:${port}/graphql`;
-  await createTestData();
-});
-
-afterAll(async (): Promise<void> => {
-  await cleanTestData();
-});
-
-beforeEach(async (): Promise<void> => {
-  graphQLClient = new GraphQLClient(getHost());
-});
-
-describe("User", (): void => {
-
-  it("should be able to get all users if user is logged in and if it has role `ADMIN`",
-    async (): Promise<void> => {
-      const adminUserEmail = "perica@gmail.com";
-      const userLoginMutationVariables = {
-        user: {
-          email: adminUserEmail,
-          password: "password"
-        }
-      };
-      const loginResponse = await graphQLClient.request(login, userLoginMutationVariables);
-
-      graphQLClient.setHeader('authorization', 'Bearer ' + loginResponse.login.token);
-
-      const usersQueryResult = await graphQLClient.request(users);
-      expect(usersQueryResult).toHaveProperty("users");
-      expect(usersQueryResult.users.length).toBe(2);
-    });
-
-  it("should not be able to get all users if user is not logged in",
-    async (): Promise<void> => {
-      const usersQueryResult = async () => {
-        await graphQLClient.request(users);
-      };
-
-      await expect(usersQueryResult).rejects.toThrow("Not Authorised!");
-    });
-
-  it("should not be able to get all users if user does not have role `ADMIN`",
-    async (): Promise<void> => {
-      const userEmail = "jovica@gmail.com";
-      const userLoginMutationVariables = {
-        user: {
-          email: userEmail,
-          password: "password"
-        }
-      };
-      const loginResponse = await graphQLClient.request(login, userLoginMutationVariables);
-
-      graphQLClient.setHeader('authorization', 'Bearer ' + loginResponse.login.token)
-
-      const usersQueryResult = async () => {
-        await graphQLClient.request(users)
-      };
-      await expect(usersQueryResult).rejects.toThrow("Not Authorised!");
-    });
-});
+// import {GraphQLClient} from "graphql-request";
+// import {AddressInfo} from "net";
+// import {App} from "../src/startServer";
+// import {users} from "./graphql";
+//
+// let getHost = (): string => "";
+// let graphQLClient: GraphQLClient;
+//
+// beforeAll(async (): Promise<void> => {
+//   const app = await App();
+//   const {port} = app.address() as AddressInfo;
+//   getHost = () => `http://localhost:${port}/graphql`;
+//   // await createTestData();
+// });
+//
+// // afterAll(async (): Promise<void> => {
+// //   // await cleanTestData();
+// // });
+//
+// beforeEach((): void => {
+//   graphQLClient = new GraphQLClient(getHost());
+// });
+//
+// describe("User", (): void => {
+//
+//   // it("should be able to get all users if user is logged in and if it has role `ADMIN`",
+//   //   async (): Promise<void> => {
+//   //     const adminUserEmail = "perica@gmail.com";
+//   //     const userLoginMutationVariables = {
+//   //       user: {
+//   //         email: adminUserEmail,
+//   //         password: "password"
+//   //       }
+//   //     };
+//   //     const loginResponse = await graphQLClient.request(login, userLoginMutationVariables);
+//   //
+//   //     graphQLClient.setHeader('authorization', 'Bearer ' + loginResponse.login.token);
+//   //
+//   //     const usersQueryResult = await graphQLClient.request(users);
+//   //     expect(usersQueryResult).toHaveProperty("users");
+//   //     expect(usersQueryResult.users.length).toBe(2);
+//   //   });
+//
+//   it("should not be able to get all users if user is not logged in",
+//     async (): Promise<void> => {
+//       const usersQueryResult = async () => {
+//         await graphQLClient.request(users);
+//       };
+//
+//       await expect(usersQueryResult).rejects.toThrow("Not Authorised!");
+//     });
+//
+//   // it("should not be able to get all users if user does not have role `ADMIN`",
+//   //   async (): Promise<void> => {
+//   //     const userEmail = "jovica@gmail.com";
+//   //     const userLoginMutationVariables = {
+//   //       user: {
+//   //         email: userEmail,
+//   //         password: "password"
+//   //       }
+//   //     };
+//   //     const loginResponse = await graphQLClient.request(login, userLoginMutationVariables);
+//   //
+//   //     graphQLClient.setHeader('authorization', 'Bearer ' + loginResponse.login.token)
+//   //
+//   //     const usersQueryResult = async () => {
+//   //       await graphQLClient.request(users)
+//   //     };
+//   //     await expect(usersQueryResult).rejects.toThrow("Not Authorised!");
+//   //   });
+// });
