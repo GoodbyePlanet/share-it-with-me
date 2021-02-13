d0f6de8deb9c57b9561b3e73ab8a61ed7d97040d
Nemanja <nemanja.vasic@codecentric.de>
2021-02-13 22:12:38 +0100

    WIP use specific test data for every test file

--- tests/post.test.ts
+++ tests/post.test.ts
@@ -1,9 +1,10 @@
-import {request} from 'graphql-request';
+import {GraphQLClient} from 'graphql-request';
 import {posts as postsQuery} from './graphql';
 import {AddressInfo} from "net";
 import {App} from "../src/startServer";
 import {cleanTestData, createTestData} from "./testData";
 
+let graphQLClient: GraphQLClient;
 let getHost = (): string => "";
 
 beforeAll(async () => {
@@ -17,14 +18,17 @@ afterAll(async () => {
   await cleanTestData();
 });
 
+beforeEach((): void => {
+  graphQLClient = new GraphQLClient(getHost());
+});
 
 describe("Posts", (): void => {
 
   it("should get all posts", async (): Promise<void> => {
-    const postsQueryResult = await request(getHost(), postsQuery);
+    const postsQueryResult = await graphQLClient.request(postsQuery);
 
     expect(postsQueryResult).not.toBeNull();
     expect(postsQueryResult).toHaveProperty("posts");
     expect(postsQueryResult.posts.length).toBe(2);
   });
-})
+});
---
+++ tests/setup.ts
@@ -0,0 +1,15 @@
+// import {cleanTestData, createTestData} from "./testData";
+//
+// jest.setTimeout(3 * 60 * 1000);
+//
+// beforeAll(async (done) => {
+//   console.log('INSIDE BEFORE ALL TESTS');
+//   await createTestData();
+//   done();
+// });
+//
+// afterAll(async (done) => {
+//   console.log('INSIDE AFTER ALL TESTS');
+//   await cleanTestData();
+//   done();
+// });
