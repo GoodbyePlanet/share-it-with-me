03aa6135029c08314f3bcbc76eff77d1ccea8f2c
Nemanja <nemanja.vasic@codecentric.de>
2021-03-26 11:36:46 +0100

    Add signUp resolver and update seed.ts

--- src/resolvers/mutation/authentication.ts
+++ src/resolvers/mutation/authentication.ts
@@ -2,9 +2,9 @@ import * as bcrypt from "bcryptjs";
 import * as jwt from "jsonwebtoken";
 import {AuthenticationResolver} from "../../typings/resolverTypes";
 import {APP_SECRET} from "../../utils/config";
-import {UserNotFoundError, WrongCredentialsError} from "../../validation";
+import {UserNotFoundError, WrongCredentialsError, UserAlreadyExistsError} from "../../validation";
 
-// const SALT_WORK_FACTOR = 10;
+const SALT_WORK_FACTOR = 10;
 
 const signJwtToken = (userId: string, role: string): string => {
   return jwt.sign({userId, role}, APP_SECRET!);
@@ -18,7 +18,6 @@ export const Authentication: AuthenticationResolver = {
       return new UserNotFoundError;
     }
 
-    console.log("PASWORD ", user.password);
     const isValid = await bcrypt.compare(password, user.password);
     if (!isValid) {
       return new WrongCredentialsError;
@@ -26,15 +25,15 @@ export const Authentication: AuthenticationResolver = {
 
     return {token: signJwtToken(user.id, user.role), user}
   },
-  // signup: async (_, {user: userInput}, ctx) => {
-  //   const password = await bcrypt.hash(userInput.password, SALT_WORK_FACTOR);
-  //   const existingUser = await ctx.prisma.$exists.user({email: userInput.email});
-  //
-  //   if (existingUser) {
-  //     return new UserAlreadyExistsError;
-  //   }
-  //
-  //   const user = await ctx.prisma.createUser({...userInput, password});
-  //   return {token: signJwtToken(user.id, user.role), user}
-  // }
+  signup: async (_, {user: userInput}, ctx) => {
+    const password = await bcrypt.hash(userInput.password, SALT_WORK_FACTOR);
+    const existingUser = await ctx.prisma.user.findUnique({where: {email: userInput.email}});
+
+    if (existingUser) {
+      return new UserAlreadyExistsError;
+    }
+
+    const user = await ctx.prisma.user.create({data: {...userInput, password}});
+    return {token: signJwtToken(user.id, user.role), user}
+  }
 }
--- tests/testData.ts
+++ tests/testData.ts
@@ -1,68 +1,68 @@
-import {prisma} from "../src/generated/prisma-client";
-
-export const createUsers = async () => {
-  await prisma.createUser({
-    id: "user-id-1",
-    email: "perica@gmail.com",
-    username: "Perica",
-    password: "$2a$10$P9unxIUzSMkanXPkZ2sIAOaXumkFRhSNj4V7LkxpMUE9H358dfYJC",
-    role: "ADMIN"
-  });
-
-  await prisma.createUser({
-    id: "user-id-2",
-    email: "jovica@gmail.com",
-    username: "Jovica",
-    password: "$2a$10$P9unxIUzSMkanXPkZ2sIAOaXumkFRhSNj4V7LkxpMUE9H358dfYJC"
-  });
-};
-
-export const createPosts = async () => {
-  await prisma.createPost({
-    id: "post-id-1",
-    title: "NodeJS in Action",
-    content: "Some awesome content...",
-    tags: {
-      set: ["NodeJS"]
-    },
-    author: {
-      create:
-        {
-          email: "postemail@gmail.com",
-          password: "password",
-          username: "PostUsername"
-        }
-    }
-  });
-
-  await prisma.createPost({
-    id: "post-id-2",
-    title: "Java in Action",
-    content: "Some awesome content on Java programming language...",
-    tags: {
-      set: ["Java"]
-    },
-    author: {
-      create:
-        {
-          email: "postemailjava@gmail.com",
-          password: "password",
-          username: "PostUsername"
-        }
-    }
-  });
-};
-
-export const cleanPosts = async (): Promise<void> => {
-  await prisma.deletePost({id: "post-id-1"});
-  await prisma.deletePost({id: "post-id-2"});
-}
-
-export const cleanAuth = async (): Promise<void> => {
-  await prisma.deleteUser({email: "testuser@gmail.com"})
-}
-
-export const cleanUsers = async (): Promise<void> => {
-  await prisma.deleteUser({id: "user-id-1"});
-  await prisma.deleteUser({id: "user-id-2"});
-}
+// import {prisma} from "../src/generated/prisma-client";
+//
+// export const createUsers = async () => {
+//   await prisma.createUser({
+//     id: "user-id-1",
+//     email: "perica@gmail.com",
+//     username: "Perica",
+//     password: "$2a$10$P9unxIUzSMkanXPkZ2sIAOaXumkFRhSNj4V7LkxpMUE9H358dfYJC",
+//     role: "ADMIN"
+//   });
+//
+//   await prisma.createUser({
+//     id: "user-id-2",
+//     email: "jovica@gmail.com",
+//     username: "Jovica",
+//     password: "$2a$10$P9unxIUzSMkanXPkZ2sIAOaXumkFRhSNj4V7LkxpMUE9H358dfYJC"
+//   });
+// };
+//
+// export const createPosts = async () => {
+//   await prisma.createPost({
+//     id: "post-id-1",
+//     title: "NodeJS in Action",
+//     content: "Some awesome content...",
+//     tags: {
+//       set: ["NodeJS"]
+//     },
+//     author: {
+//       create:
+//         {
+//           email: "postemail@gmail.com",
+//           password: "password",
+//           username: "PostUsername"
+//         }
+//     }
+//   });
+//
+//   await prisma.createPost({
+//     id: "post-id-2",
+//     title: "Java in Action",
+//     content: "Some awesome content on Java programming language...",
+//     tags: {
+//       set: ["Java"]
+//     },
+//     author: {
+//       create:
+//         {
+//           email: "postemailjava@gmail.com",
+//           password: "password",
+//           username: "PostUsername"
+//         }
+//     }
+//   });
+// };
+//
+// export const cleanPosts = async (): Promise<void> => {
+//   await prisma.deletePost({id: "post-id-1"});
+//   await prisma.deletePost({id: "post-id-2"});
+// }
+//
+// export const cleanAuth = async (): Promise<void> => {
+//   await prisma.deleteUser({email: "testuser@gmail.com"})
+// }
+//
+// export const cleanUsers = async (): Promise<void> => {
+//   await prisma.deleteUser({id: "user-id-1"});
+//   await prisma.deleteUser({id: "user-id-2"});
+// }
