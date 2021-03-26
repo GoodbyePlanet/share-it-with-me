c385a107c4b1a9fd5bb38b58f98ab3624769c537
Nemanja <nemanja.vasic@codecentric.de>
2021-03-26 10:46:16 +0100

    Add getUserById resolver implementation

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -2,6 +2,6 @@ import {QueryResolver} from "../typings/resolverTypes";
 
 export const Query: QueryResolver = {
   users: (_, __, ctx) => ctx.prisma.user.findMany(),
-  // userById: (_, {id}, ctx) => ctx.prisma.user({id}),
+  userById: (_, {id}, ctx) => ctx.prisma.user.findUnique({where: {id}}),
   // posts: (_, __, ctx) => ctx.prisma.posts()
 }
