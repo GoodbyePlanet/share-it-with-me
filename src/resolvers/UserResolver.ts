e9edce12941f41835df2a45858af8242cbf1f114
Nemanja <nemanja.vasic@codecentric.de>
2021-04-24 15:12:27 +0200

    Add userById query

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,9 +1,17 @@
-import {Authorized, Ctx, FieldResolver, Query, Resolver, Root} from "type-graphql";
+import {Arg, Authorized, Ctx, FieldResolver, Query, Resolver, Root} from "type-graphql";
+import {User as PrismaUser} from "@prisma/client";
+
 import {User} from "../model/User";
 import {Context} from "../context";
 
 @Resolver(User)
 export class UserResolver {
+
+  @Authorized("ADMIN", "USER")
+  @Query(() => User, {nullable: true})
+  async userById(@Ctx() ctx: Context, @Arg("id") id: string): Promise<PrismaUser | null> {
+    return ctx.prisma.user.findUnique({where: {id}});
+  }
 
   @Authorized("ADMIN")
   @Query(() => [User])
