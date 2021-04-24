020f43f565b0de48aa562975aa293d5cd5c0b870
Nemanja <nemanja.vasic@codecentric.de>
2021-04-24 16:33:51 +0200

    Add signUp mutation

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,9 +1,53 @@
-import {Arg, Authorized, Ctx, FieldResolver, Query, Resolver, Root, UseMiddleware} from "type-graphql";
-import {User as PrismaUser} from "@prisma/client";
+import {
+  Arg,
+  Authorized,
+  Ctx,
+  Field,
+  FieldResolver,
+  InputType,
+  Mutation,
+  ObjectType,
+  Query,
+  Resolver,
+  Root,
+  UseMiddleware
+} from "type-graphql";
+import * as bcrypt from "bcryptjs";
+import * as jwt from "jsonwebtoken";
 
 import {NotFound} from "../middlewares/notFound";
 import {User} from "../model/User";
 import {Context} from "../context";
+import {APP_SECRET} from "../utils/config";
+import {UserAlreadyExistsError} from "../errorHandling";
+
+const SALT_WORK_FACTOR = 10;
+const signJwtToken = (userId: string, role: string): string => {
+  return jwt.sign({userId, role}, APP_SECRET!);
+}
+
+@InputType()
+class CreateUserInput {
+
+  @Field()
+  email: string;
+
+  @Field()
+  username: string;
+
+  @Field()
+  password: string;
+}
+
+@ObjectType()
+class SignUpResponse {
+
+  @Field()
+  token: string
+
+  @Field(() => User)
+  user: User
+}
 
 @Resolver(User)
 export class UserResolver {
@@ -11,7 +55,7 @@ export class UserResolver {
   @Authorized("ADMIN", "USER")
   @UseMiddleware(NotFound)
   @Query(() => User, {nullable: true})
-  async userById(@Ctx() ctx: Context, @Arg("id") id: string): Promise<PrismaUser | null> {
+  async userById(@Ctx() ctx: Context, @Arg("id") id: string) {
     return ctx.prisma.user.findUnique({where: {id}});
   }
 
@@ -24,6 +68,19 @@ export class UserResolver {
   @FieldResolver()
   async posts(@Root() user: User, @Ctx() ctx: Context) {
     return ctx.prisma.user.findUnique({where: {id: user.id}}).posts();
+  }
+
+  @Mutation(() => SignUpResponse, {name: "signUp"})
+  async createUser(@Arg('createUserInput') createUserInput: CreateUserInput, @Ctx() ctx: Context) {
+    const password = await bcrypt.hash(createUserInput.password, SALT_WORK_FACTOR);
+    const existingUser = await ctx.prisma.user.findUnique({where: {email: createUserInput.email}});
+
+    if (existingUser) {
+      throw UserAlreadyExistsError;
+    }
+
+    const user = await ctx.prisma.user.create({data: {...createUserInput, password}});
+    return {token: signJwtToken(user.id, user.role), user}
   }
 
 }
