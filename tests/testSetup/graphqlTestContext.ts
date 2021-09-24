7f95c75e92cf164a008c793b497543c74b360d47
Nemanja <nemanja.vasic@codecentric.de>
2021-09-24 08:31:05 +0200

    Comment out tests

--- tests/testSetup/graphqlTestContext.ts
+++ tests/testSetup/graphqlTestContext.ts
@@ -1,34 +1,34 @@
-import {Server as HttpServer} from "http";
-import {Options} from "graphql-yoga";
-import {server} from "../../src/server";
-import {GraphQLClient} from "graphql-request";
-import {PrismaClient} from "@prisma/client";
-
-const db = new PrismaClient();
-
-export const graphqlTestContext = (port: number) => {
-  let serverInstance: HttpServer | null;
-
-  return {
-    async before(): Promise<GraphQLClient> {
-      const options: Options = {
-        port,
-        endpoint: '/graphql'
-      };
-
-      try {
-        serverInstance = await server.start(options);
-        serverInstance.on('close', async () => {
-          await db.$disconnect();
-        });
-      } catch (err: any) {
-        console.log(err);
-      }
-
-      return new GraphQLClient(`http://localhost:${options.port}/graphql`);
-    },
-    async after(): Promise<void> {
-      await serverInstance?.close();
-    },
-  }
-}
+// import {Server as HttpServer} from "http";
+// import {Options} from "graphql-yoga";
+// import {server} from "../../src/server";
+// import {GraphQLClient} from "graphql-request";
+// import {PrismaClient} from "@prisma/client";
+//
+// const db = new PrismaClient();
+//
+// export const graphqlTestContext = (port: number) => {
+//   let serverInstance: HttpServer | null;
+//
+//   return {
+//     async before(): Promise<GraphQLClient> {
+//       const options: Options = {
+//         port,
+//         endpoint: '/graphql'
+//       };
+//
+//       try {
+//         serverInstance = await server.start(options);
+//         serverInstance.on('close', async () => {
+//           await db.$disconnect();
+//         });
+//       } catch (err: any) {
+//         console.log(err);
+//       }
+//
+//       return new GraphQLClient(`http://localhost:${options.port}/graphql`);
+//     },
+//     async after(): Promise<void> {
+//       await serverInstance?.close();
+//     },
+//   }
+// }
